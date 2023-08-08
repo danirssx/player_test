493fa604c081e8f544cc12488e9ea8ee05631692
Daniel <rossdaniel5018@gmail.com>
2023-08-08 03:20:06 -0400

    Test dropping index Levels

--- helpers.py
+++ helpers.py
@@ -23,4 +23,30 @@ class BotScrap:
             # It's important to close the scraper when you're done with it. Otherwise, you'll have a bunch of webdrivers open and running
             scraper.close()
 
+        ############################################################
+
+        data = {
+            'home': [],
+            'away': [],
+            'shots': []
+        }
+
+        # Fixing Multi-Index
+
+        try:
+            lg_table['Home Player Stats'][0].values[0][1].columns = lg_table['Home Player Stats'][0].values[0][1].columns.droplevel(
+                0)
+            lg_table['Home Player Stats'][0].values[0][1] = lg_table['Home Player Stats'][0].values[0][1].dropna()
+            print(lg_table['Home Player Stats'][0].values[0][1])
+
+        except IndexError:
+            return None
+
         return lg_table
+
+
+# Try errors
+
+# dataMap = BotScrap()
+# dataMap.get_match(
+#     'https://fbref.com/en/matches/2e4383ca/Arsenal-Leeds-United-April-1-2023-Premier-League')
