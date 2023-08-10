5e7c259ce3d89e760a70c83f053b491ca8d52814
Daniel <rossdaniel5018@gmail.com>
2023-08-10 17:50:27 -0400

    Made it stats_extra()

--- fbref.py
+++ fbref.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import ScraperFC as sfc
 from bs4 import BeautifulSoup
+import html5lib
 import traceback
 
 # class where I will save all the data
@@ -9,11 +10,12 @@ import traceback
 
 class BotScrap:
 
-    def reset_multi_index(self, dataset, rename=True, start_index=0, drop=True):
+    # Reset Multi-Index
+    def reset_multi_index(self, dataset, rename=True, start_index=0, drop=True, level=0):
 
         for i in range(start_index, len(dataset)):
             try:
-                dataset[i].columns = dataset[i].columns.droplevel(0)
+                dataset[i].columns = dataset[i].columns.droplevel(level)
                 if drop:
                     dataset[i] = dataset[i].dropna()
                 dataset[i].reset_index(drop=True, inplace=True)
@@ -27,7 +29,62 @@ class BotScrap:
 
         return dataset
 
-    ####################
+    ######
+    # get Stats:
+
+    def get_stats(self, url):
+        # Get data from match
+        scraper = sfc.FBRef()
+        response = scraper.requests_get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # scrap stats
+        stats_data = soup.find_all('div', {'id': 'team_stats'})
+
+        print(stats_data)
+
+        if len(stats_data) == 1:
+            stats_data = pd.read_html(str(stats_data[0]))[0]
+            stats_data = stats_data[~stats_data.isna().all(axis=1)]
+        else:
+            stats_data = None
+
+        barata = stats_data.copy()
+
+        # # Necessary provisional
+        list_data = [stats_data, barata]
+        self.reset_multi_index(list_data, rename=False, drop=False, level=1)
+
+        return stats_data
+
+    ######
+    # get Stats Extra:
+
+    def get_stats_extra(self, url):
+        # Get data from match
+        scraper = sfc.FBRef()
+        response = scraper.requests_get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # scrap stats
+        stats_data = soup.find_all('div', {'id': 'team_stats_extra'})
+
+        final_data = []
+
+        if len(stats_data) == 1:
+            for div in stats_data[0].find_all(lambda tag: tag.name == 'div' and not tag.attrs):
+                # Structure the data inside
+                print(div)
+                text = div.get_text(strip=True)
+                if text and len(text) < 25:
+                    final_data.append(text)
+        else:
+            stats_data = None
+
+        df = pd.DataFrame(final_data)
+
+        return df
+
     # This get_shots() code belongs to ScraperFC
 
     def get_shots(self, url, home=None, away=None):
@@ -35,6 +92,9 @@ class BotScrap:
         scraper = sfc.FBRef()
         response = scraper.requests_get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
+
+        # # Scrap stats
+        # stats_data = soup.find_all('div', {'id': 'team_stats'})
 
         # Scrap shots
         both_shots = soup.find_all('table', {'id': 'shots_all'})
@@ -45,7 +105,7 @@ class BotScrap:
             both_shots = both_shots[~both_shots.isna().all(axis=1)]
 
         else:
-            both_shots: None
+            both_shots = None
 
         # Home
         if home != None:
@@ -122,5 +182,5 @@ class BotScrap:
 # Try errors
 
 # dataMap = BotScrap()
-# dataMap.get_match(
+# dataMap.get_stats_extra(
 #     'https://fbref.com/en/matches/2e4383ca/Arsenal-Leeds-United-April-1-2023-Premier-League')
